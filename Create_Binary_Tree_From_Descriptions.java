263a478fbb6243e7c249649e04dc4af8627e16b4
omnhinge <omhinge.in@gmail.com>
2026-06-08 00:43:24 +0530

    2196

--- Create_Binary_Tree_From_Descriptions.java
+++ Create_Binary_Tree_From_Descriptions.java
@@ -15,6 +15,32 @@
  */
 class Solution {
     public TreeNode createBinaryTree(int[][] descriptions) {
-        
+        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer , TreeNode> map = new HashMap<>();
+        for(int desc[] : descriptions){
+            //for parent node
+            if(!map.containsKey(desc[0])){
+                map.put(desc[0],new TreeNode(desc[0]));
+            }
+            //for child node 
+            if(!map.containsKey(desc[1])){
+                map.put(desc[1],new TreeNode(desc[1]));
+            }
+            // making link with direction
+            if(desc[2]==1){
+                map.get(desc[0]).left = map.get(desc[1]);
+            }else {
+                map.get(desc[0]).right = map.get(desc[1]);
+            }
+            set.add(desc[1]);
+        }
+        // for root node
+        for(int desc[] : descriptions){
+            if(!set.contains(desc[0])){
+                return map.get(desc[0]);
+            }
+
+        }
+        return null;
     }
 }
