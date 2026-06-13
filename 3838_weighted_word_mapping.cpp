0f4f1d8ee9bad5dcbb88fdd75c0d2741e30f3d84
omnhinge <omhinge.in@gmail.com>
2026-06-13 23:17:12 +0530

    3838 cpp

---
+++ 3838_weighted_word_mapping.cpp
@@ -0,0 +1,16 @@
+// 3838. Weighted Word Mapping
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans = "";
+        for(int i =0;i<words.size();i++){
+            int sum = 0;
+            for(int j=0;j<words[i].size();j++){
+                sum += weights[words[i][j] -'a'];
+            }
+            sum = (26-sum%26-1);
+            ans += 'a'+sum;
+        }
+        return ans;
+    }
+};
