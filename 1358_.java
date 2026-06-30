2ca65b260dd44d00636d275bde04f08505108757
Om Hinge <omhinge.in@gmail.com>
2026-06-30 23:48:23 +0530

    Create 1358_.java

---
+++ 1358_.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int n = s.length();
+        int left = 0;
+        int ans = 0;
+
+        for (int right = 0; right < n; right++) {
+            char ch = s.charAt(right);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            while (map.size() == 3) {
+                ans += (n - right);
+
+                char leftChar = s.charAt(left);
+                map.put(leftChar, map.get(leftChar) - 1);
+
+                if (map.get(leftChar) == 0) {
+                    map.remove(leftChar);
+                }
+
+                left++;
+            }
+        }
+
+        return ans;
+    }
+}
